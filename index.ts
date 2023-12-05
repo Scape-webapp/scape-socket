d588834599c1e3ffee2b3b9b9a30a49b6d10dbf2
poonam-kumawat <mailpoonam2002@gmail.com>
2023-12-05 23:06:42 +0530

    installed socket

--- index.ts
+++ index.ts
@@ -1,14 +1,31 @@
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import { createServer } from "http";
+import * as http from "http";
+import * as socketio from "socket.io";
 
 dotenv.config();
 const app: Express = express();
+const httpServer = createServer(app);
 const port = process.env.PORT;
+const server: http.Server = http.createServer(app);
+const io: socketio.Server = new socketio.Server();
+io.attach(server);
+
 const allowedOrigins = ["http://localhost:3000"];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
+
+io.on("connection", (socket: socketio.Socket) => {
+  console.log("connection");
+  socket.emit("status", "Hello from Socket.io");
+
+  socket.on("disconnect", () => {
+    console.log("client disconnected");
+  });
+});
 
 app.use(express.static("public"));
 app.use(cors(options));
