afb33197abf79dcd1af64d16a837d811763e7363
poonam-kumawat <mailpoonam2002@gmail.com>
2023-12-14 00:28:21 +0530

    connection

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { createServer } from "http";
 import * as http from "http";
-import * as socketio from "socket.io";
 import { SOCKET_EVENTS } from "./utils/common";
 import { onConnection } from "./controllers/message.controller";
 import { connectToMongo } from "./conn";
+import { Server } from "socket.io";
 
 dotenv.config();
 connectToMongo();
@@ -21,16 +21,6 @@ const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
 
-export const io = new socketio.Server(httpServer, {
-  cors: {
-    credentials: true,
-    origin: "*",
-  },
-  maxHttpBufferSize: 1e8, //* 100 MB
-});
-
-io.on(SOCKET_EVENTS.CONNECTION, onConnection);
-
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
@@ -38,6 +28,14 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Scape Socket Repository");
 });
+export const io = new Server(httpServer, {
+  cors: {
+    credentials: true,
+    origin: "*",
+  },
+  maxHttpBufferSize: 1e8, //* 100 MB
+});
+io.on(SOCKET_EVENTS.CONNECTION, onConnection);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
