b557b9816db489f5843233f210a14995ecedbd74
poonam-kumawat <mailpoonam2002@gmail.com>
2024-01-30 22:41:03 +0530

    added one key in schema

--- src/models/message.schema.ts
+++ src/models/message.schema.ts
@@ -4,6 +4,7 @@ interface Message {
   sender: any;
   receiver: any;
   text: string;
+  image:string;
   archive: any;
 }
 const messageSchema = new Schema<Message>(
@@ -11,6 +12,7 @@ const messageSchema = new Schema<Message>(
     sender: { type: mongoose.Types.ObjectId, required: true },
     receiver: { type: mongoose.Types.ObjectId, required: true },
     text: { type: String },
+    image:{type:String},
     archive: {
       type: [mongoose.Types.ObjectId],
       default: [],
